7f70ecc9d132e4ec81e3798407421cdded1c229e
kbeloin <61366520+kbeloin@users.noreply.github.com>
2021-09-13 16:34:28 -0700

    hku

--- server/settings.py
+++ server/settings.py
@@ -18,7 +18,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = Path(__file__).resolve().root.root
 
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
